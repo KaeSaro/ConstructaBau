788b0890f7de6443695abb7d4177ec56fd3d1bf2
KaeSar <e.keser21@googlemail.com>
2024-12-11 18:35:18 +0100

    testImg

--- app/team/page.js
+++ app/team/page.js
@@ -43,7 +43,7 @@ export default function Team() {
     {
       name: 'Eyüp Keser',
       position: 'Polier',
-      image: '/eyo.jpg',
+      image: 'https://picsum.photos/329/174',
     },
     {
       name: 'Don Carlos',
