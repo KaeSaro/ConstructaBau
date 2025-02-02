3830db59b976a485997f580db1098643530c5b8f
KaeSar <e.keser21@googlemail.com>
2025-02-02 14:49:02 +0100

    Performance improvments for news page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const headersList = await headers();
   const protocol = headersList.get('x-forwarded-proto') || 'https'; 
   const host = headersList.get('host');
-    const baseUrl = `${protocol}://${host}`
+  const baseUrl = `${protocol}://${host}`
 
   return [
     {
